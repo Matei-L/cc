ebf8727e78ea4bbd9e6cb63f830f59d3b28d7ff4
Matei-L <lipanmatei@gmail.com>
2020-03-23 21:52:44 +0200

    solved the directory bug

---
+++ Google App Engine Flex/app.js
@@ -0,0 +1,53 @@
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START gae_flex_quickstart]
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const sensitive = require('./sensitive.js');
+
+var whitelist = [sensitive.localClient(), sensitive.client()];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+app.get('/',cors(corsOptions), (req, res) => {
+  res
+    .status(200)
+    .header("Access-Control-Allow-Origin", "*")
+    .json({
+      "message": "Hello ;)",
+      "origin": sensitive.serverApi()
+    })
+    .end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_flex_quickstart]
+
+module.exports = app;
