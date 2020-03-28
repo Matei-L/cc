1e959f1e73e1f714251e529b00d6bdcf17136746
mariusBlaj47 <47899839+mariusBlaj47@users.noreply.github.com>
2020-03-28 21:20:53 +0200

    bugs bugs bugs

--- tema3/client/src/app/users/users-list/users-list.component.ts
+++ tema3/client/src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-users-list',
@@ -8,12 +9,13 @@ import {Component, OnInit} from '@angular/core';
 export class UsersListComponent implements OnInit {
   games: Array<string>;
 
-  constructor() {
+  constructor( private cdRef: ChangeDetectorRef) {
     this.games = [];
     this.games.push('Game #1', 'Game #2', 'Game #3');
   }
 
   ngOnInit(): void {
+    this.cdRef.detectChanges();
   }
 
 
