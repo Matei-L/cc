0abca4c54c6b5f7b8983b53a61bcfb4d1fdff665
Matei-L <lipanmatei@gmail.com>
2020-03-29 11:52:05 +0300

    I have splitted the server into smaller pieces

---
+++ tema3/api/functions/speechToText.js
@@ -0,0 +1,36 @@
+const app = require("./expressWrapper")();
+const speech = require('@google-cloud/speech');
+const speechClient = new speech.SpeechClient();
+
+app.post('/', async (req, res) => {
+
+    // The name of the audio file to transcribe
+    const blob = req.body;
+    const buffer = Buffer.from(blob);
+    const audioBytes = buffer.toString('base64');
+
+    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+    const audio = {
+        content: audioBytes,
+    };
+    const config = {
+        languageCode: 'en-US',
+    };
+    const request = {
+        audio: audio,
+        config: config,
+    };
+
+    // Detects speech in the audio file
+    const [response] = await speechClient.recognize(request);
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+
+    res.status(200)
+        .json({"transcription": transcription})
+        .end();
+});
+
+module.exports = app;
--- tema3/client/src/environments/environment.ts
+++ tema3/client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5001/the-boyz-56c01/europe-west1/api',
+  baseUrl: 'http://localhost:5001/the-boyz-56c01/europe-west1',
   randomAvatars: 'https://api.adorable.io/avatars/'
 };
 
